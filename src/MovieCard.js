d7de9a329e39531ca80e970806f2f4b5a55a328d
Javi <javi@jsrois.net>
2021-11-29 18:25:22 +0100

    show a tag for non available movies

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -4,8 +4,10 @@ export const MovieCard = ({ movie, onClick }) => {
             className="movieCard"
             id={`movie-${movie.id}`}>
             <div className="movieCard__img">
+                { movie.booked && <div class="bookedSign">No disponible </div>}
                 <img src={movie.coverImage}></img>
             </div>
+            
             <div className="movieCard__details">
                 <div className="title">{`${movie.title} (${movie.year})`}</div>
                 <div className="director">{movie.director}</div>
