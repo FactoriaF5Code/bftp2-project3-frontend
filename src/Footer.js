0235a90e61109c6bd9f34623e114afd0dff70684
msastreharo <msastreharo@gmail.com>
2021-11-30 15:23:25 +0100

    Modified styles in front, font is now white

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
         <div className={"copyrightMessage"}>Copyright ©FilmCity 2021</div>
         <div className={"socialMediaLinks"}>
             <p>Síguenos en nuestras redes sociales</p>
-            <img src={socialLogos} className={"rrssImage"} />
+            <img src={socialLogos} className={"rrssImage"} alt={"social media logos"}/>
 
         </div>
     </footer>
--- src/TopBar.js
+++ src/TopBar.js
@@ -2,7 +2,7 @@ import filmCityLogo from './assets/filmCityLogo.png';
 
 export const TopBar = () => {
     return <nav className="topBar">
-        <img src={filmCityLogo} className={"logoImg"}/>
+        <img src={filmCityLogo} className={"logoImg"} alt={"FilmCity logo"}/>
         <p className={"LemaWeb"}>Nos mueve el cine</p>
     </nav>
 }
